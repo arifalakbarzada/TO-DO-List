dab489fef8fa4b7a4643218f073c8aaa30156117
arif2004js <yearif689@gmail.com>
2024-03-27 13:29:59 +0400

    Func created

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const addTodobtn = document.querySelector(".addTodo");
 const todoList = document.querySelector(".todoList");
 const modeLight  = document.getElementById("light-mode");
 const modeDark   = document.getElementById( "dark-mode" );
-
-addTodobtn.addEventListener("click", () => {
-
+let todos = [];
+const createTodo = ()=>{
     if (inputElem.value.trim() !== ``) {
         let elemLi = document.createElement("li");
         elemLi.innerText = inputElem.value;
@@ -13,25 +12,31 @@ addTodobtn.addEventListener("click", () => {
         inputElem.value = ``;
         let deleteBtn = document.createElement("button");
         deleteBtn.classList.add('delete');
-        deleteBtn.innerHTML= `<i class="fa-solid fa-trash-can"></i>`
+        deleteBtn.innerHTML= `<i class="fa-solid fa-trash-can"></i>`;
         elemLi.appendChild(deleteBtn);
         deleteBtn.addEventListener("click", function(){
             this.parentElement.remove();
-            })
-    } 
+            });
+    }
     else{
         alert("Please enter a task!");
         inputElem.value = ``;
     }
+};
+addTodobtn.addEventListener("click",createTodo);
+document.addEventListener('keyup',(e)=>{
+if (e.key === `Enter`) {
+    createTodo();
+}
 });
 modeLight.style.display = "none";
 modeDark.addEventListener( 'click', ()=>{
     document.body.classList.add( "dark" ) ;
     modeLight.style.display = "inline";
     modeDark.style.display =  "none";
-})
+});
 modeLight.addEventListener ('click', ()=>{
     document.body.classList.remove(`dark`) ;
     modeDark.style.display =  "inline";
     modeLight.style.display = "none";
-})
+});
